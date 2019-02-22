8623ed517792954231a3a8197937e0c7f38f868a
Doberman <botalov_86@list.ru>
2019-02-22 17:40:02 +0500

    Д/з к 3 уроку

---
+++ Homework/hw03/task01_easy.py
@@ -0,0 +1,15 @@
+def my_round(number, ndigits):
+    int_ndigits = int(ndigits)
+    degree = pow(10,int(ndigits))
+    mul =  number*degree
+    res = int(mul)
+    ost = mul-res
+    # print(number,mul,res,ost)
+    if not (abs(ost) < 0.5):
+        if res>0: res+=1
+        else: res-=1
+    return res/degree
+
+
+
+my_round(2.1234567, 5)
